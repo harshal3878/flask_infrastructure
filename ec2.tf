3db16d2197161345b5ab27563289e00083fb4784
foodLover123 <muskaan.thakur@hotelkeyapp.com>
2023-03-20 12:35:11 +0530

    made changes to ec2 user-data

--- ec2.tf
+++ ec2.tf
@@ -7,6 +7,12 @@ resource "aws_instance" "test_terraform_instance"{
   pip3 install mysql-connector
   sudo yum install mariadb
   sudo yum install httpd
+  sudo touch /var/lib/cloud/scripts/per-boot/upload_ip.sh
+  sudo chmod 700 upload_ip.sh
+  cat <<EOF2> /var/lib/cloud/scripts/per-boot/upload_ip.sh
+  curl http://checkip.amazonaws.com | cat > /home/ec2-user/ip.txt
+  aws s3 cp ip.txt s3://harshal-terraform-bucket/ip/ip.txt
+  EOF2
   mkdir /home/ec2-user/flask
   EOF
 }
