46c7beafb0c3b0645aca353dfb404c48418d83d6
foodLover123 <muskaan.thakur@hotelkeyapp.com>
2023-03-20 12:02:26 +0530

    isntalling dependencies with help of user_data

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,12 @@
 resource "aws_instance" "test_terraform_instance"{
   ami = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
+  user_data = <<EOF
+  #!/bin/bash
+  sudo pip3 install flask
+  pip3 install mysql-connector
+  sudo yum install mariadb
+  sudo yum install httpd
+  mkdir /home/ec2-user/flask
+  EOF
 }
