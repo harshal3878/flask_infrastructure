4387473362ed26c73f514092415587f77ce9e89b
foodLover123 <muskaan.thakur@hotelkeyapp.com>
2023-03-21 10:09:17 +0530

    final infrastructure for now

--- ec2.tf
+++ ec2.tf
@@ -1,20 +1,34 @@
 resource "aws_instance" "test_terraform_instance"{
   ami = "ami-0aa7d40eeae50c9a9"
   instance_type = "t2.micro"
-  iam_instance_profile = "${aws_iam_instance_profile. test_project_S3_role.name}"
+  key_name = "harshalTomcat"
+  iam_instance_profile = aws_iam_instance_profile.s3_access_ec2_profile.name
   user_data = <<EOF
   #!/bin/bash
+  sudo mkdir /home/ec2-user/flask
+  sudo touch /home/ec2-user/ip.txt
+  curl http://checkip.amazonaws.com | cat > /home/ec2-user/ip.txt
+  aws s3 cp /home/ec2-user/ip.txt s3://harshal-terraform-bucket/ip/ip.txt
+  sudo yum install -y mariadb-server
+  sudo systemctl start mariadb
+  aws s3 cp s3://harshal-terraform-bucket/sql/schema.sql /home/ec2-user/schema.sql
+  aws s3 cp s3://harshal-terraform-bucket/sql/mysql_secure_installation /home/ec2-user/mysql_secure_installation 
+  sudo chmod 700 /home/ec2-user/mysql_secure_installation
+  sudo cp /home/ec2-user/mysql_secure_installation /bin/mysql_secure_installation 
+  sudo mysql_secure_installation
   sudo pip3 install flask
   pip3 install mysql-connector
-  aws s3 cp s3://harshal-terraform-bucket/sql/schema.sql schema.sql
-  mysql -t -vv -h localhost -u root -p"root" -A < schema.sql>output.txt
-  sudo yum install mariadb
-  sudo yum install httpd
+  touch /home/ec2-user/output.txt
+  mysql -t -vv -h localhost -u root -p"root" -A < /home/ec2-user/schema.sql>output.txt
+  sudo yum install httpd -y
+  sudo service httpd start
   sudo touch /var/lib/cloud/scripts/per-boot/upload_ip.sh
   sudo chmod 700 upload_ip.sh
   cat <<EOF2> /var/lib/cloud/scripts/per-boot/upload_ip.sh
+  export FLASK_APP=/home/ec2-user/app.py
   curl http://checkip.amazonaws.com | cat > /home/ec2-user/ip.txt
   aws s3 cp ip.txt s3://harshal-terraform-bucket/ip/ip.txt
+  sudo service httpd start
   EOF2
   mkdir /home/ec2-user/flask
   sed \'s/DocumentRoot \"\/var\/www\/html\"/DocumentRoot  \"\/home\/ec2-user\/flask\"/g\' httpd.conf > httpd.conf
@@ -22,7 +36,7 @@ resource "aws_instance" "test_terraform_instance"{
 }
 
 resource "aws_iam_instance_profile" "s3_access_ec2_profile" {
-  name  = "s3_access_ec2_profile"
-  role = "test_project_S3_role"
+  name  = "s3_access_ec2_profile2"
+  role = aws_iam_role.role.name
 }
 
